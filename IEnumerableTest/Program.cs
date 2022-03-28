9225fe412667418d4abd2f161540248e1ad1ec72
Micael Quilião Gerson <micaelquiliao20@gmail.com>
2022-03-28 16:19:27 -0300

    Adicionar arquivos de projeto.

---
+++ IEnumerableTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nomes = new List<string> { "Luis", "João", "Ricardo", "Alexandre" };
+            IEnumerable<string> nomesContenhamLetraO = nomes.Where(x => x.Contains("o"));
+            nomes[0] = "Marcos";
+
+            foreach (var nome in nomesContenhamLetraO)
+            {
+                Console.WriteLine(nome);
+            }
+            Console.ReadLine();
+        }
+    }
+}
